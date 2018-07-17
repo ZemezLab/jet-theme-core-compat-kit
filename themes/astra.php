3587e39e0797078e5a861a7a24744f560a5f5cc2
mjhead <andremjhead@gmail.com>
2018-07-17 18:43:11 +0300

    UPD: better Astra compatibility

--- themes/astra.php
+++ themes/astra.php
@@ -7,8 +7,9 @@ class JTCCK_Astra extends JTCCK_Theme_Base {
 
 	public function get_map() {
 		return array(
-			'astra_header' => 'header',
-			'astra_footer' => 'footer',
+			'astra_header'       => 'header',
+			'astra_footer'       => 'footer',
+			'astra_content_loop' => 'single',
 		);
 	}
 
