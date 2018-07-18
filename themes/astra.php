78f3f70ec99acf7e0fb8b2346630802f59aace8d
mjhead <andremjhead@gmail.com>
2018-07-18 12:47:02 +0300

    UPD: better compatibility with Astra theme

--- themes/astra.php
+++ themes/astra.php
@@ -9,8 +9,39 @@ class JTCCK_Astra extends JTCCK_Theme_Base {
 		return array(
 			'astra_header'       => 'header',
 			'astra_footer'       => 'footer',
-			'astra_content_loop' => 'single',
+			'astra_content_loop' => 'single, archive',
 		);
+	}
+
+	/**
+	 * Process single location
+	 *
+	 * @return void
+	 */
+	public function process_location() {
+
+		$hook     = current_filter();
+		$map      = $this->get_map();
+		$location = ! empty( $map[ $hook ] ) ? $map[ $hook ] : false;
+
+		if ( ! $location ) {
+			return;
+		}
+
+		if ( 'astra_content_loop' === $hook ) {
+			if ( is_singular() ) {
+				$done = jtcck()->do_location( 'single' );
+			} else {
+				$done = jtcck()->do_location( 'archive' );
+			}
+		} else {
+			$done = jtcck()->do_location( $location );
+		}
+
+		if ( $done ) {
+			remove_all_actions( $hook );
+		}
+
 	}
 
 }
