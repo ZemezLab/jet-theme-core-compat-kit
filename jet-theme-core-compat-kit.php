bb11df390e9ed0dfa7fcdfc7114c091e0451b3a3
kaskad88 <kaskad.8888@gmail.com>
2019-09-19 11:48:08 +0300

    UPD: plugin info

--- jet-theme-core-compat-kit.php
+++ jet-theme-core-compat-kit.php
@@ -5,7 +5,7 @@
  * Description:
  * Version:     1.1.0
  * Author:      Zemez
- * Author URI:  https://zemez.io/wordpress/
+ * Author URI:
  * Text Domain: jtcck
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
